f0d251e24defb810517c5015b4d1a70e909e0143
Zane Matero <materozane@gmail.com>
2023-03-02 00:31:50 -0800

    express set-up

---
+++ index.js
@@ -0,0 +1,19 @@
+require('dotenv').config()
+var express = require('express');
+var app = express();
+const methodOverride = require('method-override')
+
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
+app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
+
+app.get('/', (req, res) => {
+    res.render('home')
+})
+
+app.get('*', (req, res) => {
+    res.render('error')
+})
+
+app.listen(process.env.PORT, console.log(process.env.PORT))
---
+++ views/home.jsx
@@ -0,0 +1,9 @@
+const React = require('react')
+
+function home () {
+    return (
+        <main>
+            <h1>Home!</h1>
+        </main>
+    )
+}
